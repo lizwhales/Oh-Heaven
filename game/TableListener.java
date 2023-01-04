bbebf4188ba7190ff22c4443263d5daaf9bb267a
lizwhales <70874436+lizwhales@users.noreply.github.com>
2023-01-04 20:50:55 +1100

    Delete game directory

--- game/TableListener.java
+++
@@ -1,8 +0,0 @@
-package oh_heaven.game;
-
-import ch.aplu.jcardgame.*;
-
-public interface TableListener {
-    // Observer interface to be used by CardObserver
-    public void onCardPlay(int playerNum, Oh_Heaven.Suit lead, Card selected);
-}
